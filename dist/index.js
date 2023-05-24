4b62d00c50eca78d27a628d36ce5940a6b243355
piyaponSae <piyapon.sae@gmail.com>
2023-05-24 16:01:22 +0700

    get common word

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,36 @@
 "use strict";
 function getQuestionPart(phrases) {
-    const words = phrases;
-    for (let i = 0; i < words.length; i++) {
-        const word = words[i].trim();
-        const wordWithoutSpace = word.replace(/\s/g, "");
-        console.log(wordWithoutSpace);
+    const phrase = phrases.map((word) => word.replace(/\s/g, ""));
+    const phrase1 = phrase[0].split("");
+    const phrase2 = phrase[1].split("");
+    const phrase3 = phrase[2].split("");
+    const intersection = (arr1, arr2, arr3) => {
+        const group1 = arr1.filter((ele) => arr2.includes(ele) && arr3.includes(ele));
+        const group2 = arr2.filter((ele) => arr1.includes(ele) && arr3.includes(ele));
+        const group3 = arr3.filter((ele) => arr1.includes(ele) && arr2.includes(ele));
+        return [group1, group2, group3];
+    };
+    let allGroup = intersection(phrase1, phrase2, phrase3);
+    function extractWord(groups) {
+        const commonLettersSet = new Set(groups[0]);
+        for (let i = 0; i < groups.length; i++) {
+            const group = groups[i];
+            const groupSet = new Set(group);
+            commonLettersSet.forEach((letter) => {
+                if (!groupSet.has(letter)) {
+                    commonLettersSet.delete(letter);
+                }
+            });
+        }
+        const commonLetters = Array.from(commonLettersSet);
+        if (commonLetters.length === 0) {
+            return null;
+        }
+        const word = commonLetters.join("");
+        return word;
     }
+    console.log(extractWord(allGroup));
 }
-getQuestionPart(["BEFRIEND", "GIRLFRIEND", "FRIENDSHIP"]);
+getQuestionPart(["FRIENDSHIP", "GIRLFRIEND", "BEFRIEND"]);
+getQuestionPart(["BLOODBATH", "BATH SALTS", "BATHROOM"]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,54 @@
 function getQuestionPart(phrases: string[]): void {
-  const words: string[] = phrases;
-  for (let i = 0; i < words.length; i++) {
-    const word: string = words[i].trim();
-    const wordWithoutSpace: string = word.replace(/\s/g, "");
-    console.log(wordWithoutSpace);
-  }
-}
+  const phrase = phrases.map((word) => word.replace(/\s/g, ""));
+  const phrase1: string[] = phrase[0].split("");
+  const phrase2: string[] = phrase[1].split("");
+  const phrase3: string[] = phrase[2].split("");
 
-getQuestionPart(["BEFRIEND", "GIRLFRIEND", "FRIENDSHIP"]);
+  const intersection = (
+    arr1: string[],
+    arr2: string[],
+    arr3: string[]
+  ): any => {
+    const group1: string[] = arr1.filter(
+      (ele) => arr2.includes(ele) && arr3.includes(ele)
+    );
+    const group2: string[] = arr2.filter(
+      (ele) => arr1.includes(ele) && arr3.includes(ele)
+    );
+    const group3: string[] = arr3.filter(
+      (ele) => arr1.includes(ele) && arr2.includes(ele)
+    );
+    return [group1, group2, group3];
+  };
+
+  let allGroup = intersection(phrase1, phrase2, phrase3);
+
+  function extractWord(groups: string[][]): string | null {
+    const commonLettersSet = new Set(groups[0]);
+
+    for (let i = 0; i < groups.length; i++) {
+      const group = groups[i];
+
+      const groupSet = new Set(group);
+
+      commonLettersSet.forEach((letter) => {
+        if (!groupSet.has(letter)) {
+          commonLettersSet.delete(letter);
+        }
+      });
+    }
+
+    const commonLetter: String[] = Array.from(commonLettersSet);
+
+    if (commonLetter.length === 0) {
+      return null;
+    }
+
+    const word: string = commonLetter.join("");
+    return word;
+  }
+
+  console.log(extractWord(allGroup));
+}
+getQuestionPart(["FRIENDSHIP", "GIRLFRIEND", "BEFRIEND"]);
+getQuestionPart(["BLOODBATH", "BATH SALTS", "BATHROOM"]);
